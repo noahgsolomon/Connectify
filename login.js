7c330d6dd6ea6f2b5175ca3ea728f9a7bd9f6a60
noahsolomon <noahsolomon2003@gmail.com>
2023-05-02 19:51:53 -0400

    Bug fixes and comment features

--- login.js
+++ login.js
@@ -63,7 +63,6 @@ document.addEventListener("DOMContentLoaded", function() {
             const sessionId = await login().catch(error => console.error(error));
             if (sessionId) {
                 console.log(sessionId);
-                setCookie('JSESSIONID', sessionId, 7); // Store the session ID in a cookie for 7 days
                 // Redirect the user to the dashboard page
                 window.location.href = 'dashboard.html';
             }
